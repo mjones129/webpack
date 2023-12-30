6a1b0f97462c9e8a046748ff2bb92c4c772e2cec
Matt Jones <me@mattjones.tech>
2023-12-30 12:59:25 -0500

    lesson 30

--- src/js/api.js
+++ src/js/api.js
@@ -1,12 +1,12 @@
 export function getPics() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            const mockedResponse = [
-                'images/random/1.jpg',
-                'images/random/2.jpg',
-                'images/random/3.jpg'
-            ];
-            resolve(mockedResponse);
-        }, 700);
-    })
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const mockedResponse = [
+        "images/random/1.webp",
+        "images/random/2.webp",
+        "images/random/3.webp",
+      ];
+      resolve(mockedResponse);
+    }, 700);
+  });
 }
--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,126 +1,140 @@
-const common = require('./webpack.common.config');
-const { merge } = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const path = require('path');
-const glob = require('glob');
-const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-
+const common = require("./webpack.common.config");
+const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require("path");
+const glob = require("glob");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
-    output: {
-        filename: 'js/[name].[contenthash:12].js',
-    },
-    mode: 'production',
-    optimization: {
-        minimize: true,
-        minimizer: [
-            '...', //keep existing minimizers
-            new CssMinimizerPlugin({
-                minimizerOptions: {
-                    preset: [
-                        'default',
-                        {
-                            discardComments: {removeAll: true}
-                        }
-                    ]
-                }
-            }),
-            new ImageMinimizerPlugin({
-                minimizer: {
-                    implementation: ImageMinimizerPlugin.imageminMinify,
-                    options: {
-                        plugins: [
-                            ['imagemin-mozjpeg', {quality: 40}],
-                            ['imagemin-pngquant', {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            }],
-                            ['imagemin-gifsicle', {interlaced: true}],
-                            ['imagemin-svgo', {
-                                plugins: [
-                                    {
-                                        name: 'preset-default',
-                                        params: {
-                                            overrides: {
-                                                removeViewBox: false,
-                                                addAttributesToSVGElement: {
-                                                    params: {
-                                                        attributes: [
-                                                            { xmlns: 'https://www.w3.org/2000/svg' }
-                                                        ]
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                ]
-                            }]
-                        ]
-                    }
-                }
-            })
-        ]
-    },
-    module: {
-        rules: [
+  output: {
+    filename: "js/[name].[contenthash:12].js",
+  },
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      "...", //keep existing minimizers
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            "default",
             {
-                test: /\.css$/,
-                exclude: /\.module\.css$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader']
+              discardComments: { removeAll: true },
             },
-            {
-                test: /\.css$/,
-                include: /\.module\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                localIdentName: '[hash:base64]'
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader', 
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(jpg|svg|png)$/,
-                type: 'asset',
-                parser: {
-                    dataUrlCondition:{
-                        maxSize: 10 * 1024,
-                    }
+          ],
+        },
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["imagemin-mozjpeg", { quality: 40 }],
+              [
+                "imagemin-pngquant",
+                {
+                  quality: [0.65, 0.9],
+                  speed: 4,
                 },
-                generator: {
-                    filename: './images/[name].[contenthash:12][ext]'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:12].css'
-        }),
-        new PurgeCSSPlugin({
-            paths: glob.sync(
-                `${path.join(__dirname, '../src/')}/**/*`,
-                {nodir: true}
-            )
-        })
-    ]
-})
+              ],
+              ["imagemin-gifsicle", { interlaced: true }],
+              [
+                "imagemin-svgo",
+                {
+                  plugins: [
+                    {
+                      name: "preset-default",
+                      params: {
+                        overrides: {
+                          removeViewBox: false,
+                          addAttributesToSVGElement: {
+                            params: {
+                              attributes: [
+                                { xmlns: "https://www.w3.org/2000/svg" },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+        generator: [
+          {
+            type: "asset",
+            preset: "webp-custom-name",
+            implementation: ImageMinimizerPlugin.imageminGenerate,
+            options: {
+              plugins: ["imagemin-webp"],
+            },
+          },
+        ],
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.css$/,
+        include: /\.module\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[hash:base64]",
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|svg|png)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024,
+          },
+        },
+        generator: {
+          filename: "./images/[name].[contenthash:12][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:12].css",
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${path.join(__dirname, "../src/")}/**/*`, {
+        nodir: true,
+      }),
+    }),
+  ],
+});
