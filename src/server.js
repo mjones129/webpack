48ea1d1b7ce1d61af216744a0f2f5136f769b374
Matt Jones <me@mattjones.tech>
2024-01-31 21:13:54 -0500

    configure express to serve gzipped assets

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const expressStaticGzip = require('express-static-gzip');
 
 if (process.env.NODE_ENV === 'dev') {
   console.log('development mode');
@@ -22,7 +23,7 @@ app.get('/', function(req, res) {
   res.sendFile(absPath);
 })
 
-app.use('/static', express.static(path.resolve(__dirname, '../dist/')))
+app.use('/static', expressStaticGzip(path.resolve(__dirname, '../dist/')))
 
 app.listen(3000, function () {
   console.log('App is on http://localhost:3000');
